8e6a49f8d649673427d284b7edc6954a90b06412
Victor Rozhkov <victorrozhkov@icloud.com>
2019-04-08 23:11:15 +0300

    Variables

---
+++ Java projects/The First Java Program/Example.java
@@ -0,0 +1,5 @@
+class Example {
+    public public static void main(String args[]) {
+        System.out.println("Java правит Интернетом");
+    }
+}
---
+++ Variables/src/Example2.java
@@ -0,0 +1,15 @@
+public class Example2 {
+    public static void main(String args[]) {
+        int var1; // variable is decared
+        int var2; // another variable is declared
+        var1 = 1024; // var1 is assigned a value
+
+        System.out.println("Переменная var1 содержит " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("Переменная var2 содержит var1 / 2: ");
+        System.out.println(var2);
+
+    }
+}
